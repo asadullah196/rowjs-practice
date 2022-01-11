386ff59b2a2bed558848b951b8ceb062fd25bda1
Asadullah Galib <md.asadullah196@gmail.com>
2022-01-11 13:37:26 +0600

    Testing on the way with JS

--- myPractice.js
+++ myPractice.js
@@ -10,3 +10,8 @@ function bodyChangeA() {
     document.getElementById('demo-body1').innerHTML = '15 Old ' + 20;
     document.getElementById('demo-body2').innerHTML = '15 New ' + 20;
 }
+
+function bodyChangeB() {
+    document.getElementById('demo-body1').innerHTML = 'Reseted';
+    document.getElementById('demo-body2').innerHTML = '<h1>Reseted</h1>';
+}
