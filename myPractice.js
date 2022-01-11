f56d4c0b408cdb37390838390728e52a5bb3f4c1
Asadullah Galib <md.asadullah196@gmail.com>
2022-01-11 12:08:13 +0600

    External JS added in the file and see how it works.

---
+++ myPractice.js
@@ -0,0 +1,9 @@
+function bodyChange() {
+    document.getElementById('Paragraph-1').innerHTML = 'By by XpeedStudio!';
+}
+
+function bodyChangeC() {
+    document.getElementById('Paragraph-1').innerHTML = 'Hello Asadullah!';
+}
+
+
