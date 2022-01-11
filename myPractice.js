27994427c79eb46ec26237af910653ec148010d4
Asadullah Galib <md.asadullah196@gmail.com>
2022-01-11 13:32:13 +0600

    Code block testing!

--- myPractice.js
+++ myPractice.js
@@ -6,4 +6,7 @@ function bodyChangeC() {
     document.getElementById('Paragraph-1').innerHTML = '15 ' + 20;
 }
 
-
+function bodyChangeA() {
+    document.getElementById('demo-body1').innerHTML = '15 Old ' + 20;
+    document.getElementById('demo-body2').innerHTML = '15 New ' + 20;
+}
