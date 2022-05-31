b71d6158419f517e23ff7f96a23d7c866770ec9d
Asadullah Galib <md.asadullah196@gmail.com>
2022-06-01 00:26:23 +0600

    Var tested and need to declare first to get output

--- js/index3.js
+++ js/index3.js
@@ -2,9 +2,8 @@ document.getElementById("classTwo").innerHTML = "Mariya!";
 
 let galib = "Mariya!";
 
-
-
 {
     let ga = "I miss you!@";
-    document.getElementById("classThree").innerHTML = ga;
+    var gal = "I miss you!@";
 }
+document.getElementById("classThree").innerHTML = gal;
