6a4a0031ea0e566a5df7c08bead96a0cbb7259b8
Asadullah Galib <md.asadullah196@gmail.com>
2022-06-01 00:20:20 +0600

    block tested

--- js/index3.js
+++ js/index3.js
@@ -1,3 +1,10 @@
 document.getElementById("classTwo").innerHTML = "Mariya!";
 
 let galib = "Mariya!";
+
+
+
+{
+    let ga = "I miss you!@";
+    document.getElementById("classThree").innerHTML = ga;
+}
