46822a0bbb90f18a1742a6dc3ad35f7eb1f64b6c
Asadullah Galib <md.asadullah196@gmail.com>
2022-08-15 20:55:08 +0600

    Fore Loop Done

--- js/practice3.js
+++ js/practice3.js
@@ -1,1 +1,8 @@
-const cars = ["A", "b", "C", "D", "E", "F", "G"];
+const cars = ["A", "B", "C", "D", "E", "F", "G"];
+
+for(let i = 0; i<cars.length; i++){
+    console.log("Array elements position" + i + " and Elements = ", cars[i]);
+    console.log("=============");
+}
+
+console.log("I am done!");
