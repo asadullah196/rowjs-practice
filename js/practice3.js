5c69009937c6824d329bf848ce63bf7c86f2642b
Asadullah Galib <md.asadullah196@gmail.com>
2022-08-15 19:37:21 +0600

    Array declared

--- js/practice3.js
+++ js/practice3.js
@@ -1,14 +1,1 @@
-function oilPrice(number1, number2, number3){
-
-    let dijel =number1;
-    let petrol = number2;
-   let octen = number3;
-
-   result = dijel*114 + petrol*130 + octen*135;
-   document.getElementById("price").innerHTML = result;
-
-   return result;
-
-}
-
-console.log(oilPrice(1,0,2));
+const cars = ["A", "b", "C", "D", "E", "F", "G"];
