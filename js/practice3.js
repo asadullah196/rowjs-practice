6f32fa8b129bba9e754759df241a2e8f4727a7ff
Asadullah Galib <md.asadullah196@gmail.com>
2022-08-15 21:01:41 +0600

    For In loop done

--- js/practice3.js
+++ js/practice3.js
@@ -1,8 +1,19 @@
 const cars = ["A", "B", "C", "D", "E", "F", "G"];
 
 for(let i = 0; i<cars.length; i++){
-    console.log("Array elements position" + i + " and Elements = ", cars[i]);
-    console.log("=============");
+    console.log(cars[i]);
 }
 
 console.log("I am done!");
+
+const myName = {
+    fname: "Asad",
+    lname: "Galib",
+    age: 25,
+    set: "Dhaka",
+    year: 20.53,
+}
+
+for( let x in myName){
+    console.log(myName[x]);
+}
