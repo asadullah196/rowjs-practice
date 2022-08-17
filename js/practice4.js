89ce70bd5b07a92cc59e0536958532c95f57b864
Asadullah Galib <md.asadullah196@gmail.com>
2022-08-17 21:17:13 +0600

    Try and Catch Added

--- js/practice4.js
+++ js/practice4.js
@@ -1,2 +1,23 @@
-let asad = "Asadullah";
-console.log(asad);
+// Try Catch test with self made error
+
+let myInput = "";
+
+try {
+    if(myInput == ""){
+        throw "Empty";
+    }
+    if(isNaN(myInput)){
+        throw "Not a number";
+    }
+    if(myInput < 5){
+        throw "Too small";
+    }
+    if(myInput > 5){
+        throw "Too big";
+    }
+}
+catch(err){
+    
+}
+
+console.log("Made by galib");
