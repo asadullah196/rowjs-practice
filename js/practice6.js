88e480f4afee7c84d50add06a94e3bcff44d47b6
Asadullah Galib <md.asadullah196@gmail.com>
2022-08-20 12:57:48 +0600

    Class testing started here

--- js/practice6.js
+++ js/practice6.js
@@ -1,3 +1,15 @@
+// JS Class
+
+class myCar {
+    constructor(name, year, owner, price){
+        this.name = name;
+        this.year = year;
+        this.owner = owner;
+        this.price = price;
+    }
+}
+
+console.log(new myCar("AUDI", 2022, "Galib", 2.5 + "cr"));
 //Arrow function
 
 myFunction = () => {
