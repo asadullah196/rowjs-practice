eea7df1d6353723213ed9c4b04776d7620b99ea5
Asadullah Galib <md.asadullah196@gmail.com>
2022-08-21 23:01:14 +0600

    Comparsion mistake checked

--- js/practice7.js
+++ js/practice7.js
@@ -3,3 +3,23 @@ let lastName = "Ullah";
 
 let fullName = firstName + " " + lastName;
 console.log(fullName);
+
+// Comparison
+
+let firstNumber  = 10, secondNumber = "10";
+
+let totalNumber = firstNumber + secondNumber;
+
+console.log("Total = " + totalNumber);
+console.log(typeof firstNumber);
+console.log(typeof secondNumber);
+
+if (firstNumber == secondNumber) {
+    console.log("Both are equal!")
+}
+
+if (firstNumber === secondNumber) {
+    console.log("Both are equal number!")
+} else {
+    console.log("Hello Galib");
+}
