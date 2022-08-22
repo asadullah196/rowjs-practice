ca4bef9d73c0b5841953a7fd9d31029028a2bb45
Asadullah Galib <md.asadullah196@gmail.com>
2022-08-22 20:18:54 +0600

    repeated words counted

--- js/practice8.js
+++ js/practice8.js
@@ -1,2 +1,11 @@
+// Practice Problem
+
+const myName = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
+
+const matches = myName.match(/lorem/gi);
+console.log(matches);
+
+console.log("lorem repeation = " + matches.length);
+
 // JS practice started here
 console.log("Asad");
