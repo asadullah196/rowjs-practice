bade7dbc8bf6d32e2abf58547346cd6ee5dfa4e4
Asadullah Galib <md.asadullah196@gmail.com>
2022-09-14 22:10:25 +0600

    Loop working fine!

--- js/index1.js
+++ js/index1.js
@@ -2,6 +2,9 @@
 
 const myArray = [1, 2, 3, 4, 5, 6,];
 
+let arrayLenth = myArray.length, myNumber = 1;
+
 for (let i = 0; i < myArray.length; i++ ) {
-    console.log(myArray[i]);
+    console.log(myNumber);
+    myNumber *= 5;
 }
