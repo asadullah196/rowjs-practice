81187ccd7169ec3d3147eaddf69665e7026bd9d9
Asadullah Galib <md.asadullah196@gmail.com>
2022-10-02 18:24:24 +0600

    Function expression tested

--- js/index1.js
+++ js/index1.js
@@ -2,4 +2,11 @@ function myFunction() {
     console.log("Galib!");
 }
 
+// Store function into variable
+
+var  x = function myFunctionOne(a, b, c) {
+    return a * b - c;
+};
+
 myFunction();
+console.log(x(10,4,6));
