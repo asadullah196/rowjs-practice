073df7b8d1f3dbfc4793ebed943602007f62662b
Asadullah Galib <md.asadullah196@gmail.com>
2022-10-02 17:20:03 +0600

    Function tested

--- js/index1.js
+++ js/index1.js
@@ -1,10 +1,5 @@
-// Print a loop
-
-const myArray = [1, 2, 3, 4, 5, 6,];
-
-let arrayLenth = myArray.length, myNumber = 1;
-
-for (let i of myArray ) {
-    console.log(myNumber);
-    myNumber *= 3;
+function myFunction() {
+    console.log("Galib!");
 }
+
+myFunction();
