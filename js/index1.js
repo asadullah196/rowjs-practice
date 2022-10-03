dbd77675bdcb91b549f8488dcebe261af5074e68
Asadullah Galib <md.asadullah196@gmail.com>
2022-10-03 22:16:52 +0600

    Code improved

--- js/index1.js
+++ js/index1.js
@@ -3,8 +3,8 @@ function display(some) {
 }
 
 function calculator(num1, num2) {
-    let sum = num1 + num2;
-    return sum;
+    return sum = num1 + num2;
+    //return sum;
 }
 
 let result = calculator(5, 8);
