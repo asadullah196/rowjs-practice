5287856c77f1d563817d389ef59b12cc107878b4
Asadullah Galib <md.asadullah196@gmail.com>
2022-10-06 14:21:32 +0600

    Unknown changes

--- js/index1.js
+++ js/index1.js
@@ -1,6 +1,7 @@
 console.log("Line 1 Code");
 
-setTimeout(function(){
-console.log("Line 2 Code");
+setTimeout(function () {
+    console.log("Line 2 Code");
 }, 2000);
+
 console.log("Line 3 Code");
